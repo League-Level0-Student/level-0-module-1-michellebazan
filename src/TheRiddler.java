2375db5d914fd84e11df1ad2b359913105b21429
codeclub <codeclub@smms-rm23-31.smusd.local>
2018-02-07 15:00:40 -0800

    Riddler and XGeeks

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,17 +10,32 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String input = JOptionPane.showInputDialog(null, "What kind of tree can you carry in your hand?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+if(input.equalsIgnoreCase("A palm")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	score = score + 1; ////// score ++ only adds 1
+}
 
 		// 5. Otherwise, say "wrong" and tell them the answer
 
+else{JOptionPane.showMessageDialog(null, "Wrong, the anser was A Palm");
+}
 		// 6. Add some more riddles
+String input2 = JOptionPane.showInputDialog(null, "Feed me and I live, yet give me a drink and I die.");
 
+if(input2.equalsIgnoreCase("Fire")) {
+JOptionPane.showMessageDialog(null, "Correct!");
+score = score + 1; ////// score ++ only adds 1
+}
+
+
+else{JOptionPane.showMessageDialog(null, "Wrong, the anser was fire");
+}
 		// 2. Make a pop up to show the score.
-		
+JOptionPane.showMessageDialog(null, "The score is "+ score);
 	}
 }
 
--- src/XGeeks.java
+++ src/XGeeks.java
@@ -1,10 +1,7 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
-
-
-import javax.swing.JOptionPane;
 
 /*
 * Everyone has a superpower. Mine is writing recipes. This program will store
@@ -15,11 +12,20 @@ public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
+String Adrien = "Superspeed";
+String Laysa = "Sausage/Ditto";
+String Shani = "https://www.google.com/search?client=safari&rls=en&q=genericism&ie=UTF-8&oe=UTF-8";
+String Michelle = "River";
+String Facunio = "Sharigan/Sasuke with shadow clone";
+String Eduardo = "Time manipulation/Magic";
+String Andrew = "Teleportation";
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
+String input = JOptionPane.showInputDialog(null, "What is your name?");	
 
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+if (input.equals("Adrien")) {
+JOptionPane.show
 	}
 }
 
